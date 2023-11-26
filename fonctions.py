108a4c29c9d0aa46ff5ae83d7a46dc12a6f72fba
Keuch.lua <91921821+Keuchnotkush@users.noreply.github.com>
2023-11-26 22:08:57 +0100

    Yes

--- fonctions.py
+++ fonctions.py
@@ -15,12 +15,14 @@ def extract_names(repertoire):
     L = []
     for fichier in os.listdir(repertoire):      
         
-        if "1" in fichier:
-            print(fichier.removeprefix("Nomination_").removesuffix("1.txt"))
-        elif "2" in fichier:
-            print(fichier.removeprefix("Nomination_").removesuffix("2.txt"))
-        else:
-            print(fichier.removeprefix("Nomination_").removesuffix(".txt"))
+        if "1" in fichier and fichier.removeprefix("Nomination_").removesuffix("1.txt") not in L:
+            L.append(fichier.removeprefix("Nomination_").removesuffix("1.txt"))
+        elif "2" in fichier and fichier.removeprefix("Nomination_").removesuffix("2.txt") not in L:
+            L.append(fichier.removeprefix("Nomination_").removesuffix("2.txt"))
+        elif "1" not in fichier and "2" not in fichier and fichier.removeprefix("Nomination_").removesuffix(".txt") not in L:
+            L.append(fichier.removeprefix("Nomination_").removesuffix(".txt"))
+    for e in L:
+        print(e)
 
         
 
