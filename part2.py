337563c550b75ee979bbd4156cb4f8089d93c398
strike1510 <151172451+strike1510@users.noreply.github.com>
2023-12-22 09:35:15 +0100

    part 2
    tf idf de la question

--- part2.py
+++ part2.py
@@ -1,5 +1,5 @@
-import os, TF, math
-
+import os, math
+import TF
 punc = '''!()-[]{};:'",<>./?@#$%^&*_~'''
 
 def token(sentence):
@@ -27,49 +27,18 @@ def identify(sentence , repertoire):
     return keep
 
 def questtfidf(question):
-    ListeMots = token(question)
-    
-    quest = question.replace("'", " ")
-    quest = quest.replace(".", "")
-    quest = quest.replace(",", "")
-    quest = quest.replace(";", "")
-    quest = quest.replace("!", "")
-    quest = quest.replace("?", "")
-    quest = quest.replace("-", " ")
-    quest = quest.lower()
+
+    quest = ""
+    for e in token(question):
+        quest += e + " "
+    identify = (quest , "cleaned")
     occMots = TF.calcul_occurrences(quest)
-
-    matrice_tf_idf = []
-    mots_uniques = set()
-    nbfichier = 0
-    for fichier in os.listdir("cleaned"):
-        nbfichier += 1
-        with open(os.path.join("cleaned", fichier), 'r',encoding="utf8") as f:
-            contenu = f.read()
-            occurrences = TF.calcul_occurrences(contenu)
-            mots_uniques.update(occurrences.keys())
-            matrice_tf_idf.append(occurrences)
-    
-    mots_uniques = sorted(list(mots_uniques))
-    
-    L2 = []
-    for fichier in os.listdir("cleaned"):
-        L2.append(fichier)
-    result = {}
-    for i in range(len(mots_uniques)):
-        L20 = []
-        for j in range(len(L2)):
-            
-            idfscore = TF.calcul_score_idf("cleaned")
-            tfscore = TF.tf(str("cleaned"),str(L2[j]))
-            
-            if mots_uniques[i] in tfscore:
-                L20.append(idfscore[mots_uniques[i]] * tfscore[mots_uniques[i]])
-        
-        result[mots_uniques[i]] = L20
-    #return result
-
-    L2 = {}
-    for i in range(len(occMots)):
-        if occMots[i] in result:
-            print()
+    result = TF.calcul_score_idf("cleaned")
+    L = {}
+    for i in result.keys():
+        if i in occMots:
+            x = result[i] * occMots[i]
+            L[i] = x
+        else:
+            result[i] = 0
+    return L
